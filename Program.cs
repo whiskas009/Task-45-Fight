accb74df80f31227836d4f6b7980183895738999
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-07-06 20:14:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_45_Fight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+
+            while (isWork == true)
+            {
+                Console.WriteLine("\n1. Выбрать бойцов для боя \n2. Начать драку \n3. Выход");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+
+                        break;
+
+                    case "2":
+
+                        break;
+
+                    case "3":
+
+                        break;
+
+                    case "5":
+                        isWork = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("\nНеккоректный ввод\n");
+                        break;
+                }
+            }
+        }
+    }
+
+    class 
+}
